7eebba94f53a88a3e1a0e5cb8b9872afa889cd18
Jessica Cortes <jessica.cortes@bellevuecollege.edu>
2022-05-24 23:30:43 -0700

    insurance service working.

--- CloudHybridPatientDB/InsuranceDataService/Worker.cs
+++ CloudHybridPatientDB/InsuranceDataService/Worker.cs
@@ -10,6 +10,7 @@ using System.Text.Json;
 using System.Xml.Linq;
 using Amazon.Runtime.CredentialManagement;
 using Amazon.Runtime;
+using Amazon.SQS.Model;
 
 namespace InsuranceDataService
 {
@@ -47,57 +48,62 @@ namespace InsuranceDataService
                 AmazonSQSClient sqsClient;
                 try
                 {
-                    sqsClient = new AmazonSQSClient(GetAwsCredentials("default"));
-                } catch (Exception e)
+                    sqsClient = new AmazonSQSClient(GetAwsCredentials(), Amazon.RegionEndpoint.USEast1);
+                
+                    WriteToLog("Receiving SQS Message from downward queue...");
+                    var response = await sqsClient.ReceiveMessageAsync(
+                        "https://sqs.us-east-1.amazonaws.com/926831757693/downwardQueue"
+                        );
+                    WriteToLog("Received message.");
+
+                    string dbPath = "C:\\Users\\jessi\\OneDrive\\Documents\\School\\CS455\\cloud-hybrid-patient-db\\InsuranceDatabase\\InsuranceDatabase.xml";
+                    XDocument dbDoc = XDocument.Load(dbPath);
+                    WriteToLog("Loaded xml database");
+
+                    foreach (var message in response.Messages)
+                    {
+                        WriteToLog("message: " + message.Body);
+                        Patient? patient = JsonSerializer.Deserialize<Patient>(message.Body);
+                        if (patient == null)
+                        {
+                            throw new Exception("Could not deserialize queue message into Patient object");
+                        }
+                        WriteToLog("Deserialized patient successfully");
+
+                        var element = dbDoc.Descendants()
+                            .Where(x => (string)x.Attribute("id") == patient.id.ToString())
+                            .FirstOrDefault();
+                        WriteToLog("Attempted to find patient in db.");
+
+                        PatientInsurance insurance = new PatientInsurance();
+                        insurance.id = patient.id;
+                        if (element != null)
+                        {
+                            WriteToLog("Found patient in db.  Attempting to get insurance fields");
+                            // Found insurance policy, populate insurance related fields
+                            var policyElement = element.Descendants().Where(x => x.Attribute("policyNumber") != null).FirstOrDefault();
+                            insurance.policy = policyElement.Attribute("policyNumber").Value;
+
+                            var providerElement = policyElement.Descendants().FirstOrDefault();
+                            insurance.provider = providerElement.Value;
+                        }
+
+                        WriteToLog("Deleting message from queue...");
+                        await sqsClient.DeleteMessageAsync("https://sqs.us-east-1.amazonaws.com/926831757693/downwardQueue", message.ReceiptHandle);
+
+                        // Send insurance info to upward queue
+                        WriteToLog("Sending message to queue...");
+                        string jsonMessage = JsonSerializer.Serialize<PatientInsurance>(insurance);
+                        await sqsClient.SendMessageAsync(
+                            "https://sqs.us-east-1.amazonaws.com/926831757693/upwardQueue",
+                            jsonMessage);
+                        WriteToLog("Successfully sent message.");
+                    }
+                }
+                catch (Exception e)
                 {
                     WriteToLog(e.Message);
                     throw e;
-                }
-
-                WriteToLog("Receiving SQS Message from downward queue...");
-                var response = await sqsClient.ReceiveMessageAsync(
-                    "https://sqs.us-east-1.amazonaws.com/926831757693/downwardQueue"
-                    );
-                WriteToLog("Received message.");
-
-                string dbPath = "C:\\Users\\jessi\\OneDrive\\Documents\\School\\CS455\\cloud-hybrid-patient-db\\InsuranceDatabase\\InsuranceDatabase.xml";
-                XDocument dbDoc = XDocument.Load(dbPath);
-                WriteToLog("Loaded xml database");
-
-                foreach (var message in response.Messages)
-                {
-                    WriteToLog("message: " + message.Body);
-                    Patient? patient = JsonSerializer.Deserialize<Patient>(message.Body);
-                    if (patient == null)
-                    {
-                        throw new Exception("Could not deserialize queue message into Patient object");
-                    }
-                    WriteToLog("Deserialized patient successfully");
-
-                    var element = dbDoc.Descendants()
-                        .Where(x => Int32.Parse((string)x.Attribute("id")) == patient.id)
-                        .FirstOrDefault();
-                    WriteToLog("Attempted to find patient in db.");
-
-                    PatientInsurance insurance = new PatientInsurance();
-                    insurance.id = patient.id;
-                    if (element != null)
-                    {
-                        WriteToLog("Found patient in db.  Attempting to get insurance fields");
-                        // Found insurance policy, populate insurance related fields
-                        var policyElement = element.Descendants().Where(x => x.Attribute("policyNumber") != null).FirstOrDefault();
-                        insurance.policy = policyElement.Attribute("policyNumber").Value;
-
-                        var providerElement = policyElement.Descendants().FirstOrDefault();
-                        insurance.provider = providerElement.Value;
-                    }
-
-                    // Send insurance info to upward queue
-                    WriteToLog("Sending message to queue...");
-                    string jsonMessage = JsonSerializer.Serialize<PatientInsurance>(insurance);
-                    sqsClient.SendMessageAsync(
-                        "https://sqs.us-east-1.amazonaws.com/926831757693/upwardQueue",
-                        jsonMessage).Wait();
                 }
 
                 await Task.Delay(5000, stoppingToken);
@@ -113,21 +119,12 @@ namespace InsuranceDataService
             }
         }
 
-        private static AWSCredentials GetAwsCredentials(string profileName)
+        private static SessionAWSCredentials GetAwsCredentials()
         {
-            if (String.IsNullOrEmpty(profileName))
-            {
-                throw new ArgumentNullException("profileName cannot be null or empty.");
-            }
-
-            SharedCredentialsFile credFile = new SharedCredentialsFile();
-            CredentialProfile profile = credFile.ListProfiles().Find(p => p.Name.Equals(profileName));
-            if (profile == null)
-            {
-                throw new Exception(String.Format("Profile named {0} not found.", profileName));
-            }
-
-            return AWSCredentialsFactory.GetAWSCredentials(profile, new SharedCredentialsFile());
+            return new SessionAWSCredentials(
+                "ASIA5PS32JF6YA2ZASU3", 
+                "JcXXikWbDDwqMeOmUHTqELL03+t/FEOPR6befz6b", 
+                "FwoGZXIvYXdzEKz//////////wEaDPflgAirQRKO17VFliLFAZRngVQYeybYvokojO6XI08g1hnU3FO7rw/B/Cbli+pRX9I8tzKGJFCKaEesxLSliAX/enQgEEladAcMoFtpatCLWbsfo5t0ugdMgdKodUMMLfxnTQMp5XNg2WTf0nlhqtn6TZeuopV6OLHubTmUcnjEU6HUehMYkSKdtZjF+48Jryokr9p+ty1/7XOk6QvZhSAjI/r+ygltyzClsNR2mj+yk1o18EZ+a6rOsApTAqphDjixtKon9EU0I26RomVnER2Hk4zNKNextpQGMi0Xxo1LnnPhX+3Cg4NeyAOTD4/qETFbd8me0nnsGrp8ezAty7D8V/8T88OpcEA=");
         }
     }
 }
