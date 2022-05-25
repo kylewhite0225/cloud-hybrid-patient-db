624127cc31ae949c72df342b5f21913f530e85b4
Kyle White <kylewhite0225@gmail.com>
2022-05-25 12:24:10 -0700

    Populated InsuranceDataFunction with ParseJson and event handling logic, created InsuranceData class for storing event json as an object.

--- CloudHybridPatientDB/InsuranceDataFunction/Function.cs
+++ CloudHybridPatientDB/InsuranceDataFunction/Function.cs
@@ -1,6 +1,9 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.SQSEvents;
-
+using System.Data;
+using System.Text.Json;
+using System.IO;
+using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -29,17 +32,65 @@ public class Function
     /// <returns></returns>
     public async Task FunctionHandler(SQSEvent evnt, ILambdaContext context)
     {
-        foreach(var message in evnt.Records)
+        string s3Event = evnt.Records[0].Body;
+
+        if (s3Event == null)
         {
-            await ProcessMessageAsync(message, context);
+            throw new Exception("Event is null.");
         }
+
+        InsuranceData data = ParseJson(s3Event);
+
+        if (data.provider == "" && data.policy == "")
+        {
+            Console.WriteLine("Patient with ID {0} does not have medical insurance.", data.id);
+        } else
+        {
+            Console.WriteLine("Patient with ID {0}: policy number = {1}, provider = {2}.", data.id, data.policy, data.provider);
+        }
+
+        // foreach(var message in evnt.Records)
+        // {
+        //     await ProcessMessageAsync(message, context);
+        // }
     }
 
-    private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+    /// <summary>
+    /// Parses the JSON file obtained from the s3Event and inserts the data into RDS using Npgsql.
+    /// </summary>
+    private InsuranceData ParseJson(string json)
     {
-        context.Logger.LogInformation($"Processed message {message.Body}");
+        // Console.WriteLine(doc);
+        InsuranceData? data = null;
+        try
+        {
+            Console.WriteLine("Serializing JSON");
+            // Use JsonSerializerOptions object to allow ignoring case of JSON elements
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.PropertyNameCaseInsensitive = true; // ignore case
+            // Use JsonSerializer to deserialize the JSON string into an InsuranceData object
+            data = JsonSerializer.Deserialize<InsuranceData?>(json, options);
+        }
+        catch (Exception e)
+        {
+            // TODO
+            // Does this catch if the JSON is not well formed?
+            Console.WriteLine(e.Message);
+        }
 
-        // TODO: Do interesting work based on the new message
-        await Task.CompletedTask;
+        if (json == null)
+        {
+            throw new InvalidOperationException("Event JSON was null.");
+        }
+
+        return data;
     }
+
+    // private async Task ProcessMessageAsync(SQSEvent.SQSMessage message, ILambdaContext context)
+    // {
+    //     context.Logger.LogInformation($"Processed message {message.Body}");
+    //
+    //     // TODO: Do interesting work based on the new message
+    //     await Task.CompletedTask;
+    // }
 }
---
+++ CloudHybridPatientDB/InsuranceDataFunction/InsuranceData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InsuranceDataFunction;
+
+public class InsuranceData
+{
+    public int id { get; set; }
+
+    public string provider { get; set; }
+
+    public string policy { get; set; }
+}
